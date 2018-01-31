63be06134c011b3e299ac5f1daadf7afb1e4080c
Unknown <jeremy@bytelabs.co>
2018-01-31 14:39:05 -0600

    Renamed for consistency
    Added circleci config (based on orleans.cosmosdb)

---
+++ Orleans.EventSourcing.EventStorage/EventStorageExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Orleans.EventSourcing.EventStorage
+{
+    using System;
+    using Orleans.Hosting;
+    using Orleans.Runtime.Configuration;
+
+    public static class EventStorageExtensions
+    {
+        public static ISiloHostBuilder UseEventStorageLogProvider(this ISiloHostBuilder builder, Action<EventStorageOptions> options)
+        {
+            return builder.Configure(options);
+        }
+
+        public static void AddEventStorageLogProvider(this ClusterConfiguration config, string name)
+        {
+            config.Globals.RegisterLogConsistencyProvider<EventStorageLogConsistencyProvider>(name);
+        }
+    }
+}
